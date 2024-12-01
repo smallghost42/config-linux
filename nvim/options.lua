9f0f343ec621afe1a6a81ce91b72756a9e1043f7
smallghost <fenoherymamindrainy@gmail.com>
2024-12-01 09:46:40 +0300

    add true tab option

--- nvim/options.lua
+++ nvim/options.lua
@@ -2,5 +2,11 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+--o.cursorlineopt ='both' -- to enable cursorline!
+
+o.expandtab = false
+o.shiftwidth = 4
+o.smartindent = true
+o.tabstop = 4
+o.softtabstop = 4
